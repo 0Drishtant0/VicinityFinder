466494321ab7e2a8253523f1780c2b1817468694
Drishtant singh sengar <118834647+0Drishtant0@users.noreply.github.com>
2024-10-19 19:41:20 +0530

    reduced distance to 100M

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
-const RANGE = .5; // kilometers
+const RANGE = .1; // kilometers
 
 app.use(bodyParser.json());
 
