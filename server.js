f4413aa9f87b83c34b5e503dbca61ec77e8ea5f1
Drishtant singh sengar <118834647+0Drishtant0@users.noreply.github.com>
2024-10-20 22:02:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
-const RANGE = .025; // kilometers
+const RANGE = .01; // kilometers
 
 app.use(bodyParser.json());
 
